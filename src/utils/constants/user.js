bb40d03ddae0173e2be90cc84540ce743a5c1566
ankitgothwal <ankitgothwal20@gmail.com>
2025-07-09 11:00:00 +0530

    utils added

---
+++ src/utils/constants/user.js
@@ -0,0 +1,11 @@
+const USER = {
+  JOIN: "join",
+  JOINED: "joined",
+  LEAVE: "leave",
+  MESSAGE: "message",
+  FOCUS_ON: "focus-on",
+  FOCUS_OFF: "focus-off",
+  QUESTON: "question",
+};
+
+module.exports = USER;
